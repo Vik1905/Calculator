e401dd0e939f34cd728f9282b80b539f7305033d
Viktor <viktor.uspex@gmail.com>
2023-01-28 01:52:20 +0200

    Completed calculator program

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"This is one line calculator\n" + "Enter full math expresion using this allowed operation: \"+\", \"-\", \"*\", \"/\" \"(\", \")\" and unary minus\n");
+            var calculator = new Calculator();
+            string input = Console.ReadLine();
+            input = input.Replace(" ", "");
+            Console.WriteLine($"Answer: {calculator.Calculate(calculator.SplitByOperation(input))}");
         }
     }
 }
