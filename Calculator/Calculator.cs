c59f6c50bcb175a942d8cbaccbef29e82f9350c4
Viktor <viktor.uspex@gmail.com>
2023-01-28 01:27:41 +0200

    Added class "Calculator"

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Calculator
+    {
+        public static char[] Signs = new char[] { '+', '-', '/', '*' };
+        public static Dictionary<char, int> Priopity = new Dictionary<char, int>()
+        {
+        {'+',2 },
+        {'-',2 },
+        {'/',1 },
+        {'*',1 },
+        };
+    }
+}
